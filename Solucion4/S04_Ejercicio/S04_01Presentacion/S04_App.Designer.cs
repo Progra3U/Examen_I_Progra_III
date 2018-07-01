51cff0500320f05a364b87d334f5d6287fd0a862
Carlos Perez <31164007+DeathCom@users.noreply.github.com>
2018-07-01 10:21:41 -0600

    Creacion de S04
    Se crea la estructura basica para la solucion S04_Ejercicio
    Se agregan las relacions de:
    P -> LN y E
    LN -> AD y E
    AD -> E

---
+++ Solucion4/S04_Ejercicio/S04_01Presentacion/S04_App.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace S04_01Presentacion
+{
+    partial class S04_App
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.salirDelAppToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cerrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.administracionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.finanzasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.proveduriaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modulo01ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modulo02ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modulo03ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modulo01ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.modulo02ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.modulo01ToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.modulo02ToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.modulo03ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mantenimientoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.usuariosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.perfilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirDelAppToolStripMenuItem,
+            this.administracionToolStripMenuItem,
+            this.finanzasToolStripMenuItem,
+            this.proveduriaToolStripMenuItem,
+            this.mantenimientoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(111, 17);
+            this.toolStripStatusLabel1.Text = "Usuario Conectado:";
+            // 
+            // salirDelAppToolStripMenuItem
+            // 
+            this.salirDelAppToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cerrarToolStripMenuItem});
+            this.salirDelAppToolStripMenuItem.Name = "salirDelAppToolStripMenuItem";
+            this.salirDelAppToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.salirDelAppToolStripMenuItem.Text = "Salir Del app";
+            // 
+            // cerrarToolStripMenuItem
+            // 
+            this.cerrarToolStripMenuItem.Name = "cerrarToolStripMenuItem";
+            this.cerrarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cerrarToolStripMenuItem.Text = "Cerrar";
+            // 
+            // administracionToolStripMenuItem
+            // 
+            this.administracionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.modulo01ToolStripMenuItem,
+            this.modulo02ToolStripMenuItem,
+            this.modulo03ToolStripMenuItem});
+            this.administracionToolStripMenuItem.Name = "administracionToolStripMenuItem";
+            this.administracionToolStripMenuItem.Size = new System.Drawing.Size(100, 20);
+            this.administracionToolStripMenuItem.Text = "Administracion";
+            // 
+            // finanzasToolStripMenuItem
+            // 
+            this.finanzasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.modulo01ToolStripMenuItem1,
+            this.modulo02ToolStripMenuItem1});
+            this.finanzasToolStripMenuItem.Name = "finanzasToolStripMenuItem";
+            this.finanzasToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.finanzasToolStripMenuItem.Text = "Finanzas";
+            // 
+            // proveduriaToolStripMenuItem
+            // 
+            this.proveduriaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.modulo01ToolStripMenuItem2,
+            this.modulo02ToolStripMenuItem2,
+            this.modulo03ToolStripMenuItem1});
+            this.proveduriaToolStripMenuItem.Name = "proveduriaToolStripMenuItem";
+            this.proveduriaToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.proveduriaToolStripMenuItem.Text = "Proveduria";
+            // 
+            // modulo01ToolStripMenuItem
+            // 
+            this.modulo01ToolStripMenuItem.Name = "modulo01ToolStripMenuItem";
+            this.modulo01ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.modulo01ToolStripMenuItem.Text = "Modulo 01";
+            // 
+            // modulo02ToolStripMenuItem
+            // 
+            this.modulo02ToolStripMenuItem.Name = "modulo02ToolStripMenuItem";
+            this.modulo02ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.modulo02ToolStripMenuItem.Text = "Modulo 02";
+            // 
+            // modulo03ToolStripMenuItem
+            // 
+            this.modulo03ToolStripMenuItem.Name = "modulo03ToolStripMenuItem";
+            this.modulo03ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.modulo03ToolStripMenuItem.Text = "Modulo 03";
+            // 
+            // modulo01ToolStripMenuItem1
+            // 
+            this.modulo01ToolStripMenuItem1.Name = "modulo01ToolStripMenuItem1";
+            this.modulo01ToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.modulo01ToolStripMenuItem1.Text = "Modulo 01";
+            // 
+            // modulo02ToolStripMenuItem1
+            // 
+            this.modulo02ToolStripMenuItem1.Name = "modulo02ToolStripMenuItem1";
+            this.modulo02ToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.modulo02ToolStripMenuItem1.Text = "Modulo 02";
+            // 
+            // modulo01ToolStripMenuItem2
+            // 
+            this.modulo01ToolStripMenuItem2.Name = "modulo01ToolStripMenuItem2";
+            this.modulo01ToolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.modulo01ToolStripMenuItem2.Text = "Modulo 01";
+            // 
+            // modulo02ToolStripMenuItem2
+            // 
+            this.modulo02ToolStripMenuItem2.Name = "modulo02ToolStripMenuItem2";
+            this.modulo02ToolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.modulo02ToolStripMenuItem2.Text = "Modulo 02";
+            // 
+            // modulo03ToolStripMenuItem1
+            // 
+            this.modulo03ToolStripMenuItem1.Name = "modulo03ToolStripMenuItem1";
+            this.modulo03ToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.modulo03ToolStripMenuItem1.Text = "Modulo 03";
+            // 
+            // mantenimientoToolStripMenuItem
+            // 
+            this.mantenimientoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.usuariosToolStripMenuItem,
+            this.perfilesToolStripMenuItem});
+            this.mantenimientoToolStripMenuItem.Name = "mantenimientoToolStripMenuItem";
+            this.mantenimientoToolStripMenuItem.Size = new System.Drawing.Size(101, 20);
+            this.mantenimientoToolStripMenuItem.Text = "Mantenimiento";
+            // 
+            // usuariosToolStripMenuItem
+            // 
+            this.usuariosToolStripMenuItem.Name = "usuariosToolStripMenuItem";
+            this.usuariosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.usuariosToolStripMenuItem.Text = "Usuarios";
+            // 
+            // perfilesToolStripMenuItem
+            // 
+            this.perfilesToolStripMenuItem.Name = "perfilesToolStripMenuItem";
+            this.perfilesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.perfilesToolStripMenuItem.Text = "Perfiles";
+            // 
+            // S04_App
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "S04_App";
+            this.Text = "S04_App";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem salirDelAppToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cerrarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem administracionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modulo01ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modulo02ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modulo03ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem finanzasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modulo01ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem modulo02ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem proveduriaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modulo01ToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem modulo02ToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem modulo03ToolStripMenuItem1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripMenuItem mantenimientoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem usuariosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem perfilesToolStripMenuItem;
+    }
+}
---
+++ Solucion4/S04_Ejercicio/S04_01Presentacion/S04_Perfiles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace S04_01Presentacion
+{
+    public partial class S04_Perfiles : Form
+    {
+        public S04_Perfiles()
+        {
+            InitializeComponent();
+        }
+    }
+}
