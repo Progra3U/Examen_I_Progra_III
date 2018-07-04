ddcee77d27685c1ada3801411e918bcc58d6887d
MarcelaSalas <40038653+MarcelaSalas@users.noreply.github.com>
2018-07-03 23:13:07 -0600

    ejercicio4 info pendiente de buscar el errror
    hay un error en sql o la declaracion en logica de negocios pendiente de revisar manana

--- Solucion4/S04_Ejercicio/S04_01Presentacion/S04_Perfiles.Designer.cs
+++ Solucion4/S04_Ejercicio/S04_01Presentacion/S04_Perfiles.Designer.cs
@@ -28,12 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvPerfiles = new System.Windows.Forms.DataGridView();
+            this.cboEstado = new System.Windows.Forms.ComboBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPerfiles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvPerfiles
+            // 
+            this.dgvPerfiles.AllowUserToAddRows = false;
+            this.dgvPerfiles.AllowUserToDeleteRows = false;
+            this.dgvPerfiles.BackgroundColor = System.Drawing.Color.BlanchedAlmond;
+            this.dgvPerfiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPerfiles.Location = new System.Drawing.Point(235, 21);
+            this.dgvPerfiles.Name = "dgvPerfiles";
+            this.dgvPerfiles.ReadOnly = true;
+            this.dgvPerfiles.RowHeadersVisible = false;
+            this.dgvPerfiles.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvPerfiles.Size = new System.Drawing.Size(307, 237);
+            this.dgvPerfiles.TabIndex = 17;
+            // 
+            // cboEstado
+            // 
+            this.cboEstado.FormattingEnabled = true;
+            this.cboEstado.Items.AddRange(new object[] {
+            "Seleccione",
+            "Activo",
+            "Inactivo"});
+            this.cboEstado.Location = new System.Drawing.Point(97, 150);
+            this.cboEstado.Name = "cboEstado";
+            this.cboEstado.Size = new System.Drawing.Size(100, 21);
+            this.cboEstado.TabIndex = 16;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(97, 87);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 15;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(97, 34);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigo.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Estado:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Nombre:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Codigo:";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.Gold;
+            this.btnEliminar.Location = new System.Drawing.Point(373, 292);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 20;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.BackColor = System.Drawing.Color.Gold;
+            this.btnModificar.Location = new System.Drawing.Point(219, 292);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 19;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = false;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.Color.Gold;
+            this.btnAgregar.Location = new System.Drawing.Point(62, 292);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 18;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // S04_Perfiles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.BurlyWood;
+            this.ClientSize = new System.Drawing.Size(554, 346);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dgvPerfiles);
+            this.Controls.Add(this.cboEstado);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "S04_Perfiles";
             this.Text = "S04_Perfiles";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPerfiles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvPerfiles;
+        private System.Windows.Forms.ComboBox cboEstado;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnAgregar;
     }
 }
--- Solucion4/S04_Ejercicio/S04_01Presentacion/S04_Usuarios.cs
+++ Solucion4/S04_Ejercicio/S04_01Presentacion/S04_Usuarios.cs
@@ -7,14 +7,151 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using S04_04Entidades;
+using S04_02LogicaNegocio;
 
 namespace S04_01Presentacion
 {
     public partial class S04_Usuarios : Form
     {
+
+        private void CargarUsuarios()
+        {
+            try
+            {
+                DataTable dt = new DataTable(); //Variable de tipo tabla en memoria
+
+                //Definir las columnas a mostrar
+                dt.Columns.Add("Usuario");
+                dt.Columns.Add("Clave");
+                dt.Columns.Add("Estado");
+
+                //Carga en memoria registros de BD
+                List<Usuarios> lstusuarios = Logica.ObtenerUsuarios();
+
+                if (dgvUsuarios.Rows.Count > 0)
+                {
+                    dgvUsuarios.DataSource = null; //Limpia el gridview
+                    dgvUsuarios.Refresh();
+                }
+
+                foreach (Usuarios item in lstusuarios)
+                {
+                    string estado = String.Empty;
+                    if (item.activo)
+                        estado = "Activo";
+                    else
+                        estado = "Inactivo";
+                    //Aqui se agrega a la Tabla en memoria el registro
+                    dt.Rows.Add(item.nombreUsuario, item.pass, estado);
+                }
+                this.dgvUsuarios.DataSource = dt; //Se asigna la tabla formateada al grid
+                this.dgvUsuarios.Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void Limpiar()
+        {
+            this.txtUsuario.Text = String.Empty;
+            this.txtClave.Text = String.Empty;
+            this.cboEstado.SelectedIndex = 0;
+            this.txtUsuario.Focus();
+        }
         public S04_Usuarios()
         {
             InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Usuarios objusuario = new Usuarios();
+
+                objusuario.nombreUsuario = txtUsuario.Text.Trim();
+                objusuario.pass = txtClave.Text.Trim();
+                if (this.cboEstado.Text.Equals("Activo"))
+                    objusuario.activo = true;
+                else
+                    objusuario.activo = false;
+
+                S04_02LogicaNegocio.Logica.AgregarUsuario(objusuario);
+                MessageBox.Show("Usuario guardado");
+                CargarUsuarios();
+                Limpiar();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Usuarios objusuario = new Usuarios();
+
+                objusuario.nombreUsuario = txtUsuario.Text.Trim();
+                objusuario.pass = txtClave.Text.Trim();
+                if (this.cboEstado.Text.Equals("Activo"))
+                    objusuario.activo = true;
+                else
+                    objusuario.activo = false;
+
+                S04_02LogicaNegocio.Logica.ModificarUsuarios(objusuario);
+                MessageBox.Show("Usuario actualizado");
+                CargarUsuarios();
+                Limpiar();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Usuarios objusuario = new Usuarios();
+
+                objusuario.nombreUsuario = txtUsuario.Text.Trim();
+
+               S04_02LogicaNegocio.Logica.EliminarUsuarios(objusuario);
+                MessageBox.Show("Usuario eliminado");
+                CargarUsuarios();
+                Limpiar();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        
+    }
+        private void dgvUsuarios_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dgvUsuarios_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                this.txtUsuario.Text = dgvUsuarios.Rows[e.RowIndex].Cells[0].Value.ToString();
+                this.txtClave.Text = dgvUsuarios.Rows[e.RowIndex].Cells[1].Value.ToString();
+                if (dgvUsuarios.Rows[e.RowIndex].Cells[2].Value.ToString().Equals("Activo"))
+                    this.cboEstado.Text = "Activo";
+                else
+                    this.cboEstado.Text = "Inactivo";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
